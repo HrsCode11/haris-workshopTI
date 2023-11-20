b9925d1d09a9e409b6f0ff0900989e4d4bdbb318
Haris C <hariscahya35@gmail.com>
2023-11-20 08:53:17 +0700

    push workshop2023

---
+++ public/script.js
@@ -0,0 +1,1 @@
+alert("hallo, salam kenal XD");
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="workshop-container">
+        <div class="workshop-text">workshop2023</div>
+    </div>
+    <div class="profile">
+        <h2>Nama : Haris Cahyana</h2>
+        <h2>NIM : 233307050</h2>
+        <h2>Prodi : TI</h2>
+        <h2>Kelas : 1B</h2>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
